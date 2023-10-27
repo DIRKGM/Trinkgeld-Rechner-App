b236e91d8cd87442b8a0a9c2ab91505d8f6d921b
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-10-27 13:59:42 +0200

    Erster Entwurf

--- Trinkgeld Rechner App/ViewController.swift
+++ Trinkgeld Rechner App/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    
+    @IBOutlet weak var betragTextField: UITextField!
+    @IBOutlet weak var personenAnzahlTextField: UITextField!
+    @IBOutlet weak var prozentLabel: UITextField!
+    @IBOutlet weak var prozentSlider: UISlider!
+    @IBOutlet weak var trinkgeldLabel: UITextField!
+    @IBOutlet weak var gesamtkostenLabel: UITextField!
+    @IBOutlet weak var KostenProPersonLabel: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
